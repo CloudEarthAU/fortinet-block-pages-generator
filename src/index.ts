54a85ad857f70d968707b6d1297a2ae2d1d2ec2d
Ingo Wolf <me@ingowolf.au>
2025-08-07 19:07:53 +1000

    Add CLI input validation and file handling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import commander from "commander";
+import colors from "colors";
+import fs from "fs";
+import path from "path";
 
 const program = new commander.Command();
 program
   .version("1.0.0")
   .description("A CLI tool to create blocked pages for Fortigate firewalls")
-  .option("-l, --live", "run a live webserver to see changes in realtime")
-  .option("-f, --file <file>", "input file")
+  .option(
+    "-l, --live [port]",
+    "run a live webserver to see changes in realtime",
+  )
   .option("-b, --build", "build block pages")
+  .option("-f, --file <file>", "input file")
   .option("-o, --output <folder>", "output folder")
   .argument("[file]", "input file");
 
@@ -14,4 +20,34 @@ program.parse();
 
 const options = program.opts();
 const args = program.args;
+const action = options.build ? "build" : "live";
 const file = options.file || args[0];
+const output = options.output || file.split(".").slice(0, -1).join(".");
+console.log(output);
+const port = options.live || 3000;
+
+if (!file) {
+  console.error(colors.red("No input file provided"));
+  process.exit(1);
+}
+
+if (!fs.existsSync(file)) {
+  console.error(colors.red(`File ${file} does not exist`));
+  process.exit(1);
+}
+
+if (action === "build") {
+  if (!output) {
+    console.error(colors.red("No output folder provided"));
+    process.exit(1);
+  }
+
+  if (!fs.existsSync(output)) {
+    console.error(colors.green(`Creating output folder ${output}`));
+    fs.mkdirSync(output);
+  }
+
+  console.log(colors.green(`Building block pages from ${file}`));
+} else {
+  console.log(colors.green(`Running live webserver from ${file}`));
+}
