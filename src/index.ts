bfd2c29712a73dc317dd14ac1eec362b8481f615
Ingo Wolf <me@ingowolf.au>
2025-08-07 18:43:06 +1000

    add options

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Test");
+import commander from "commander";
+
+const program = new commander.Command();
+program
+  .version("1.0.0")
+  .description("A CLI tool to create blocked pages for Fortigate firewalls")
+  .option("-l, --live", "run a live webserver to see changes in realtime")
+  .option("-f, --file", "input file")
+  .option("-b, --build", "build block pages")
+  .option("-o, --output", "output folder");
+
+program.parse();
