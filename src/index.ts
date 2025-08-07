0cd67470a94aefa300735b2fed8ba68d4f6c48ad
Ingo Wolf <me@ingowolf.au>
2025-08-07 18:51:18 +1000

    Add file argument and improve option parsing logic

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ program
   .version("1.0.0")
   .description("A CLI tool to create blocked pages for Fortigate firewalls")
   .option("-l, --live", "run a live webserver to see changes in realtime")
-  .option("-f, --file", "input file")
+  .option("-f, --file <file>", "input file")
   .option("-b, --build", "build block pages")
-  .option("-o, --output", "output folder");
+  .option("-o, --output <folder>", "output folder")
+  .argument("[file]", "input file");
 
 program.parse();
+
+const options = program.opts();
+const args = program.args;
+const file = options.file || args[0];
