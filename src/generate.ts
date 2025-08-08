19c63556552286e3a7a37b56cac62b915e8438eb
Ingo Wolf <me@ingowolf.au>
2025-08-09 08:56:17 +1000

    add page types

---
+++ src/generate.ts
@@ -0,0 +1,102 @@
+const PAGE_TYPES = [
+  {
+    name: "FortiGuard Block Page",
+    fileName: "fortiguard-block-page",
+  },
+  {
+    name: "FortiGuard Override Page",
+    fileName: "fortiguard-override-page",
+  },
+  {
+    name: "FortiGuard Quota Page",
+    fileName: "fortiguard-quota-page",
+  },
+  {
+    name: "FortiGuard Warning Page",
+    fileName: "fortiguard-warning-page",
+  },
+  {
+    name: "Application Control Block Page",
+    fileName: "application-control-block-page",
+  },
+  {
+    name: "Application Control Block Page",
+    fileName: "application-control-block-page",
+  },
+  {
+    name: "Archive Block Page",
+    fileName: "archive-block-page",
+  },
+  {
+    name: "Banned Word Block Page",
+    fileName: "banned-word-block-page",
+  },
+  {
+    name: "Block Page",
+    fileName: "block-page",
+  },
+  {
+    name: "DLP Block Page",
+    fileName: "dlp-block-page",
+  },
+  {
+    name: "EMS Threat Feed Block Page",
+    fileName: "ems-threat-feed-block-page",
+  },
+  {
+    name: "External Blocklist Block Page",
+    fileName: "external-blocklist-block-page",
+  },
+  {
+    name: "File Filter Block Page",
+    fileName: "file-filter-block-page",
+  },
+  {
+    name: "File Size Block Upload Page",
+    fileName: "file-size-block-upload-page",
+  },
+  {
+    name: "Inline Scan Incomplete Block Page",
+    fileName: "inline-scan-incomplete-block-page",
+  },
+  {
+    name: "Inline Scan Unavailable Block Page",
+    fileName: "inline-scan-unavailable-block-page",
+  },
+  {
+    name: "IPS Scan Failure Block Page",
+    fileName: "ips-scan-failure-block-page",
+  },
+  {
+    name: "IPS Sensor Block Page",
+    fileName: "ips-sensor-block-page",
+  },
+  {
+    name: "Oversized File Block Page",
+    fileName: "oversized-file-block-page",
+  },
+  {
+    name: "Virtual Patch Block Page",
+    fileName: "virtual-patch-block-page",
+  },
+  {
+    name: "Virus Block Page",
+    fileName: "virus-block-page",
+  },
+  {
+    name: "Virus Outbreak Prevention Block Page",
+    fileName: "virus-outbreak-prevention-block-page",
+  },
+  {
+    name: "Virus Upload Block Page",
+    fileName: "virus-upload-block-page",
+  },
+  {
+    name: "Web Application Firewall Block Page",
+    fileName: "web-application-firewall-block-page",
+  },
+  {
+    name: "Windows Executable Block Page",
+    fileName: "windows-executable-block-page",
+  },
+];
