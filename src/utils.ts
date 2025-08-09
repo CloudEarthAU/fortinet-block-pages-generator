67c9cab6c21a8b541b62ed7c313b4eb8fe16d05a
Ingo Wolf <me@ingowolf.au>
2025-08-09 10:54:16 +1000

    Merge template data functions into single utility

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,13 @@
 import { PAGE_TYPES, VARIABLES } from "./constants";
 
-export function getDevTemplateData(_page: keyof typeof PAGE_TYPES) {
-  const variables = Object.fromEntries(
-    Object.entries(VARIABLES).map(([key, value]) => [key, value.default]),
-  );
-  const pageData = PAGE_TYPES[_page];
-  return { variables, name: pageData.name, type: _page };
-}
-
-export function getBuildTemplateData(_page: keyof typeof PAGE_TYPES) {
+export function getTemplateData(
+  _page: keyof typeof PAGE_TYPES,
+  _type: "dev" | "build",
+) {
   const variables = Object.fromEntries(
     Object.entries(VARIABLES).map(([key, value]) => [
       key,
-      `%%${value.variable}%%`,
+      _type == "dev" ? value.default : `%%${value.variable}%%`,
     ]),
   );
   const pageData = PAGE_TYPES[_page];
