b1feb27bef6a7e6c91bab50a10404ef2f7cc88da
Ingo Wolf <me@ingowolf.au>
2025-08-09 11:59:45 +1000

    Move init function to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { Command } from "commander";
-import fs from "fs";
-import fsPromises from "fs/promises";
-import path from "path";
-import { CONFIG_FILE } from "./constants";
+import { init } from "./init";
 
 const program = new Command();
 program
@@ -29,70 +26,3 @@ program
   .option("-o, --output", "output file (default: output/index.html)");
 
 program.parse();
-
-async function init(name: string, options: any): Promise<void> {
-  console.log(`Initialising project ${name}`);
-  // Check if project already exists
-  if (fs.existsSync(name)) {
-    // Check if project is directory
-    let isDirectory = (await fsPromises.stat(name)).isDirectory();
-    if (isDirectory) {
-      // List files
-      const files = await fsPromises.readdir(name);
-      // Check if project is already initialised
-      if (files.includes(CONFIG_FILE)) {
-        if (options.force) {
-          // Runs if -f is used
-          console.log(`Project ${name} already exists, overwriting`);
-        } else {
-          console.error(
-            `${name} has already been initialised, use -f to overwrite`,
-          );
-          process.exit(1);
-        }
-      }
-      if (
-        files.includes("index.html") ||
-        files.includes("images") ||
-        files.includes("build")
-      ) {
-        if (options.force) {
-          // Runs if -f is used
-          console.log(`Folder ${name} contains conflicting files, overwriting`);
-        } else {
-          console.log(
-            `Folder ${name} contains conflicting files, use -f to overwrite`,
-          );
-          process.exit(1);
-        }
-      }
-    } else {
-      console.error(`${name} exists and is not a directory`);
-      process.exit(1);
-    }
-  } else {
-    await fsPromises.mkdir(name);
-  }
-  // Create images if it doesn't exist
-  if (!fs.existsSync(path.join(name, "images"))) {
-    await fsPromises.mkdir(path.join(name, "images"));
-  }
-  // Create build if it doesn't exist
-  if (!fs.existsSync(path.join(name, "build"))) {
-    await fsPromises.mkdir(path.join(name, "build"));
-  }
-  // Write config file
-  await fsPromises.writeFile(
-    path.join(name, CONFIG_FILE),
-    JSON.stringify(
-      {
-        main: "./index.html",
-        images: "./images",
-      },
-      null,
-      2,
-    ),
-  );
-  // Write index.html file
-  await fsPromises.writeFile(path.join(name, "index.html"), "");
-}
