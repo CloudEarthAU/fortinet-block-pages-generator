9f1eea112b7c9a4d4efa528f6df978ad024ed9c0
Ingo Wolf <me@ingowolf.au>
2025-08-15 17:04:12 +1000

    add handling for non existent folder

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,12 +1,24 @@
 import path from "path";
+import fs from "fs";
 
 export async function build(
   inputtedFolder: string,
   inputtedOutput: string,
 ): Promise<any> {
-  console.log(process.cwd());
   const folder = path.resolve(inputtedFolder || process.cwd());
-  const output = path.resolve(inputtedOutput || path.join(folder, "output"));
+  const output = path.resolve(inputtedOutput || path.join(folder, "build"));
 
-  console.log(`Building project in ${folder} to ${output}`);
+  if (!fs.existsSync(folder)) {
+    console.log(`Folder ${folder} does not exist`);
+    process.exit(1);
+  }
+
+  if (!fs.existsSync(output)) {
+    console.log(`Output folder ${output} does not exist, creating`);
+    fs.mkdirSync(output);
+  }
+
+  console.log(
+    `Building project in ./${path.relative(process.cwd(), folder)} to ./${path.relative(process.cwd(), output)}`,
+  );
 }
