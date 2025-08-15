ac596d4a0e1786da3ce61f13f07126d082631340
Ingo Wolf <me@ingowolf.au>
2025-08-15 17:00:07 +1000

    Add initial build command implementation

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,3 +1,12 @@
-export async function build(options: any): Promise<any> {
-  console.log("Not implemented");
+import path from "path";
+
+export async function build(
+  inputtedFolder: string,
+  inputtedOutput: string,
+): Promise<any> {
+  console.log(process.cwd());
+  const folder = path.resolve(inputtedFolder || process.cwd());
+  const output = path.resolve(inputtedOutput || path.join(folder, "output"));
+
+  console.log(`Building project in ${folder} to ${output}`);
 }
