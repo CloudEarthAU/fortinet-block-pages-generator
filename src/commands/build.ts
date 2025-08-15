a5fafd389a2fd20a9c92cda1472d87cc07f1a5b0
Ingo Wolf <me@ingowolf.au>
2025-08-15 19:02:11 +1000

    add comments

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -9,17 +9,19 @@ export async function build(
   const folder = path.resolve(inputtedFolder || process.cwd());
   const output = path.resolve(inputtedOutput || path.join(folder, "build"));
 
+  // Check if folder exists and is a project
   if (!fs.existsSync(folder)) {
     console.log(`Folder ${folder} does not exist`);
     process.exit(1);
   }
-
   if (!fs.existsSync(path.join(folder, CONFIG_FILE))) {
     console.log(
       "Configuration file not found, this is probably not a Fortinet Block Page Generator project",
     );
     process.exit(1);
   }
+
+  // Check if output folder exists
   if (!fs.existsSync(output)) {
     console.log(`Output folder ${output} does not exist, creating`);
     fs.mkdirSync(output);
@@ -29,6 +31,7 @@ export async function build(
     `Building project in ./${path.relative(process.cwd(), folder)} to ./${path.relative(process.cwd(), output)}`,
   );
 
+  // Loop through all page types
   Object.entries(PAGE_TYPES).forEach(([type, value]) => {
     console.log(`Building ${type}`);
     fs.writeFileSync(path.join(output, `${value.fileName}.html`), value.name);
