7acd98566f7c3db48cb1e55fbfa75b1824a68200
Ingo Wolf <me@ingowolf.au>
2025-09-06 11:10:05 +1000

    copy build code

---
+++ src/commands/dev.ts
@@ -0,0 +1,45 @@
+import path from "path";
+import fs from "fs";
+import { CONFIG_FILE, PAGE_TYPES } from "../constants";
+import generatePage from "../generatePage";
+
+export async function build(
+  inputtedFolder: string,
+  inputtedOutput: string,
+): Promise<any> {
+  const folder = path.resolve(inputtedFolder || process.cwd());
+  const output = path.resolve(inputtedOutput || path.join(folder, "build"));
+
+  // Check if folder exists and is a project
+  if (!fs.existsSync(folder)) {
+    console.log(`Folder ${folder} does not exist`);
+    process.exit(1);
+  }
+  if (!fs.existsSync(path.join(folder, CONFIG_FILE))) {
+    console.log(
+      "Configuration file not found, this is probably not a Fortinet Block Page Generator project",
+    );
+    process.exit(1);
+  }
+
+  // Check if output folder exists
+  if (!fs.existsSync(output)) {
+    console.log(`Output folder ${output} does not exist, creating`);
+    fs.mkdirSync(output);
+  }
+
+  console.log(
+    `Building project in ./${path.relative(process.cwd(), folder)} to ./${path.relative(process.cwd(), output)}`,
+  );
+
+  // Loop through all page types
+  Object.entries(PAGE_TYPES).forEach(async ([type, value]) => {
+    console.log(`Building ${type}`);
+    const page = await generatePage(
+      type as keyof typeof PAGE_TYPES,
+      "build",
+      path.join(folder, "index.html"),
+    );
+    fs.writeFileSync(path.join(output, `${value.fileName}.html`), page);
+  });
+}
