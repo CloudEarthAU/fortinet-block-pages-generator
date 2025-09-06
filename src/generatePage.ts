1437eef65f0b8fa09598b71b8748c67672d974d1
Ingo Wolf <me@ingowolf.au>
2025-09-06 10:56:17 +1000

    inline asssets

--- src/generatePage.ts
+++ src/generatePage.ts
@@ -1,23 +1,31 @@
 import Twig from "twig";
 import fs from "fs";
+import path from "path";
+import { inlineSource } from "inline-source";
 import { PAGE_TYPES, VARIABLES } from "./constants";
 import { getTemplateData } from "./utils";
 
 export default async function generatePage(
   type: keyof typeof PAGE_TYPES,
   env: "dev" | "build",
-  path: string,
+  filePath: string,
 ) {
-  if (!fs.existsSync(path)) {
-    console.log(`${path} does not exist.`);
+  if (!fs.existsSync(filePath)) {
+    console.log(`${filePath} does not exist.`);
     process.exit(1);
   }
 
-  const html = await new Promise<string>((resolve, reject) => {
-    Twig.renderFile(path, getTemplateData(type, env), (err, html) => {
-      resolve(html);
-    });
+  const directory = path.dirname(filePath);
+
+  const inlinedHtml = await inlineSource(filePath, {
+    compress: true,
+    rootpath: directory,
+    attribute: false,
   });
+
+  const html = Twig.twig({
+    data: inlinedHtml,
+  }).render(getTemplateData(type, env));
 
   return html;
 }
